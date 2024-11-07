171f64d9aaca05690ae771758d6e02f9e7c28669
VadimKochnev <donskoy_kek@mail.ru>
2024-11-07 14:17:09 +0300

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -5,25 +5,9 @@ import './index.css'
 import { ContactPage } from './routes/ContactPage'
 import { HomePage } from './routes/HomePage'
 
-// const router = createBrowserRouter([
-// 	{
-// 		path: '/',
-// 		element: <HomePage />,
-// 	},
-// 	{
-// 		path: '/home',
-// 		element: <HomePage />,
-// 	},
-// 	{
-// 		path: '/contact',
-// 		element: <ContactPage />,
-// 	},
-// ])
-
 createRoot(document.getElementById('root')!).render(
 	<StrictMode>
 		<BrowserRouter basename='/task-react'>
-			{/* <RouterProvider router={router} /> */}
 			<Routes>
 				<Route path='/' element={<HomePage />} />
 				<Route path='/home' element={<HomePage />} />
