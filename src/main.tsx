2b5eef5dc7a2512612150039c35cd81d90f64f82
VadimKochnev <donskoy_kek@mail.ru>
2024-11-07 14:15:48 +0300

    update routing

--- src/main.tsx
+++ src/main.tsx
@@ -1,27 +1,34 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import './index.css'
 import { ContactPage } from './routes/ContactPage'
 import { HomePage } from './routes/HomePage'
 
-const router = createBrowserRouter([
-	{
-		path: '/',
-		element: <HomePage />,
-	},
-	{
-		path: '/home',
-		element: <HomePage />,
-	},
-	{
-		path: '/contact',
-		element: <ContactPage />,
-	},
-])
+// const router = createBrowserRouter([
+// 	{
+// 		path: '/',
+// 		element: <HomePage />,
+// 	},
+// 	{
+// 		path: '/home',
+// 		element: <HomePage />,
+// 	},
+// 	{
+// 		path: '/contact',
+// 		element: <ContactPage />,
+// 	},
+// ])
 
 createRoot(document.getElementById('root')!).render(
 	<StrictMode>
-		<RouterProvider router={router} />
+		<BrowserRouter basename='/task-react'>
+			{/* <RouterProvider router={router} /> */}
+			<Routes>
+				<Route path='/' element={<HomePage />} />
+				<Route path='/home' element={<HomePage />} />
+				<Route path='/contact' element={<ContactPage />} />
+			</Routes>
+		</BrowserRouter>
 	</StrictMode>
 )
